cfe8cc374037ab2b31fe4f87915b822aba1b2a7b
luisguerra44 <49825068+luisguerra44@users.noreply.github.com>
2020-02-28 18:37:36 -0600

    Create formHandler.js

---
+++ src/js/formHandler.js
@@ -0,0 +1,37 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let text = document.getElementById('name').value
+
+    Client.text
+    
+
+        console.log(text)
+
+    console.log("::: Form Submitted :::")
+    fetch('http://localhost:8080/api',{
+        method: 'POST',
+       
+        body: JSON.stringify({text}),
+        headers: {"Content-Type": "application/json"}
+    })
+    .then(res => res.json())
+    .then(function(data) {
+        console.log("Updating the UI!");
+        
+        document.getElementById('text').innerHTML = `text: ${data.text} <br>`
+        document.getElementById('polarity').innerHTML = `polarity: ${data.polarity} <br>`
+        document.getElementById('polarity_confidence').innerHTML = `polarity confidence: ${data.polarity_confidence} <br>`
+        document.getElementById('subjectivity').innerHTML = `subjectivity: ${data.subjectivity} <br>`
+        document.getElementById('subjectivity_confidence').innerHTML = `subjectivity confidence: ${data.subjectivity_confidence}`
+
+        console.log(data);
+        
+    }).catch(err => console.log(err));
+
+}
+
+
+
+export { handleSubmit }
